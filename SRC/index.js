7d3d736f24301a6bf39804b0efd9c78b75aa3df8
clemanntyne <92274079+clemanntyne@users.noreply.github.com>
2021-10-31 11:28:40 -0700

    Added Current Location Feature

--- SRC/index.js
+++ SRC/index.js
@@ -17,6 +17,41 @@ let day = days[date.getDay()];
 return `${day} ${hours}:${minutes} ${ampm}`;
 }
 
+function displayWeatherCurrentLocation (response) {
+    let tempElement = document.querySelector("#temperature");
+    tempElement.innerHTML = Math.round(response.data.main.temp);
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.name;
+    let conditionElement = document.querySelector("#condition");
+    conditionElement.innerHTML = response.data.weather[0].description;
+    let cloudElement = document.querySelector("#clouds");
+    cloudElement.innerHTML = response.data.clouds.all;
+    let windElement = document.querySelector("#wind");
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+    let humidityElement = document.querySelector("#humidity");
+    humidityElement.innerHTML = Math.round(response.data.main.humidity);
+    let dateElement = document.querySelector("#date-time");
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
+    let iconElement = document.querySelector("#icon");
+    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("alt", response.data.weather[0].description); 
+
+    fahrenheitTemperature = response.data.main.temp;
+
+    getForecast(response.data.coord);
+}
+
+function showPosition(position) {
+    let apiKey = "88724523008dc9e1be18f6eb6a959b67"
+    let apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=imperial`;
+    axios.get(apiURL).then(displayWeatherCurrentLocation);
+    console.log();
+}
+
+function getCurrentPosition() {
+    navigator.geolocation.getCurrentPosition(showPosition);
+}
+
 //Formatting API string to display day name
 function formatDay(timestamp) {
     let date = new Date(timestamp * 1000);
@@ -143,6 +178,8 @@ celsiusLink.addEventListener("click", convertToMetric);
 let fahrenheitLink = document.querySelector ("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", convertToImperial);
 
+let currentLocation = document.querySelector ("#current-location");
+currentLocation.addEventListener("click", getCurrentPosition);
 
 search("San Francisco");
-displayForecast();
+//displayForecast();
